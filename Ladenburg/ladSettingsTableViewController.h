ae27bce7e9475a7f1b114aa2284ef766ce188892
sonjaka <sonja-karl@web.de>
2014-06-06 12:06:58 +0200

    - added Table View and SubViews for Settings - added ladSettingsViewController Class

---
+++ Ladenburg/ladSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ladSettingsTableViewController.h
+//  Ladenburg
+//
+//  Created by Sonja on 06.06.14.
+//  Copyright (c) 2014 DHBW Mannheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ladSettingsTableViewController : UITableViewController
+
+@end
