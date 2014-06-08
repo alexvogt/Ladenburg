f24e85b8897e78bc58aaa8dfce3a334b32a948ad
timey <timhartl89@gmail.com>
2014-06-08 23:57:01 +0200

    -fixed failure with statusbar again

--- Ladenburg/ladBeaconTracker.m
+++ Ladenburg/ladBeaconTracker.m
@@ -515,12 +515,6 @@
     ladVC.selectedSight = _selectedSight;
 }
 
-// Set Statusbarcolor to white
--(UIStatusBarStyle)preferredStatusBarStyle{
-    
-    return UIStatusBarStyleLightContent;
-}
-
 
 /*
 #pragma mark - Navigation
