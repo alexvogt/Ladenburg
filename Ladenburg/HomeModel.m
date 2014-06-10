c6d5ce47685eda643b8860f9343ba9834d9652f2
sonjaka <sonja-karl@web.de>
2014-06-11 01:00:13 +0200

    - added dhbw logo to settingspage - set location Manager timer from 2 to 5 for saving energy. - set data connection to m-ladenburg

--- Ladenburg/HomeModel.m
+++ Ladenburg/HomeModel.m
@@ -35,30 +35,35 @@
 
     
         // Download german json file
-        //jsonFileUrl = [NSURL URLWithString:@"http://m-ladenburg.de/service-lb-de.php"];
+        jsonFileUrl = [NSURL URLWithString:@"http://m-ladenburg.de/service-lb-de.php"];
         
         // Lokale Installation
-        //NSURL *jsonFileUrl = [NSURL URLWithString:@"http://localhost:8888/service-lb.php"];
-        
+        //jsonFileUrl = [NSURL URLWithString:@"http://localhost:8888/ladenburg/service-lb-de.php"];
+       
         // Locale Nework Installation
         // Replace URL with IP of Network Server
         //jsonFileUrl = [NSURL URLWithString:@"http://192.168.100.100:8888/ladenburg/service-lb-de.php"];
         
         
         // Huawei
-        jsonFileUrl = [NSURL URLWithString:@"http://192.168.1.150:8888/ladenburg/service-lb-de.php"];
+        //jsonFileUrl = [NSURL URLWithString:@"http://192.168.1.150:8888/ladenburg/service-lb-de.php"];
         
         
     } else {
-    
-        // jsonFileUrl = [NSURL URLWithString:@"http://m-ladenburg.de/service-lb-en.php"];
+        
+        // Download german json file
+        jsonFileUrl = [NSURL URLWithString:@"http://m-ladenburg.de/service-lb-en.php"];
+        
+        // Lokale Installation
+        //jsonFileUrl = [NSURL URLWithString:@"http://localhost:8888/ladenburg/service-lb-en.php"];
         
         // Locale Nework Installation
         // Replace URL with IP of Network Server
-      //  jsonFileUrl = [NSURL URLWithString:@"http://192.168.100.100:8888/ladenburg/service-lb-en.php"];
+        //jsonFileUrl = [NSURL URLWithString:@"http://192.168.100.100:8888/ladenburg/service-lb-en.php"];
+        
         
         // Huawei
-        jsonFileUrl = [NSURL URLWithString:@"http://192.168.1.150:8888/ladenburg/service-lb-en.php"];
+        //jsonFileUrl = [NSURL URLWithString:@"http://192.168.1.150:8888/ladenburg/service-lb-en.php"];
     }
     
     
@@ -127,22 +132,23 @@
         
         
         //set image property of newSight to image
-//        baseURL = @"http://ladenburg.timhartl.de";
-        
+
+        // Webspace Tim
+        // baseURL = @"http://ladenburg.timhartl.de";
         
         // m-ladenburg server
-       // baseURL = @"http://m-ladenburg.de";
+         baseURL = @"http://m-ladenburg.de";
         
         // Lokale Installation
-        //baseURL = @"http://localhost:8888/";
+        // baseURL = @"http://localhost:8888/";
         
         // Locale Nework Installation
         // Replace URL with IP of Network Server
         
         // baseURL = @"http://192.168.100.100:8888/";
         
-        
-        baseURL = @"http://192.168.1.150:8888/";
+        // Huawei
+        //baseURL = @"http://192.168.1.150:8888/";
         
         
         shortenedImageURL = [newSight.imageUrl substringFromIndex:2];
